0f5157de93c9a52875159aee0cd239fac4726f4a
Philip Hanson <grayman3@gmail.com>
2012-10-24 14:31:31 -0400

    Parsing complete

--- hookline.py
+++ hookline.py
@@ -2,6 +2,7 @@ import re
 import feedparser
 from bs4 import BeautifulSoup
 from bs4.element import Tag, NavigableString
+from datetime import date, time
 
 # Location of schedule ATOM feed
 feed_url = "http://ministryschedule.actsii.org/feeds/posts/default"
@@ -43,6 +44,13 @@ def fix_extras(soup, extras):
                 if child.string.strip().lower().startswith(x.strip().lower()):
                     child.extract()
 
+def fix_orphans(soup):
+    """ Removes break tags that separate orphaned lines from their parents """
+    for br in soup.findAll('br'):
+        s = br.nextSibling.string
+        if s and len(s) > 0 and not (re.search('\d+', s) or re.search(':', s)):
+            br.extract()
+
 def get_lines(soup):
     """ Returns the text of each break-separated line in the document,
         stripping away all HTML tags. """
@@ -55,20 +63,83 @@ def get_lines(soup):
                 lines[-1] += clean(child.string)
     return [line.strip() for line in lines]
 
+def is_header(line):
+    """ Returns True iff the given line is a header. """
+    return re.search('\d+', line) is not None
+
+def get_header_date(header):
+    """ Returns the date contained in the given header. """
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    s = re.search('(%s)\s*(\d+)' % '|'.join(months), header, re.IGNORECASE)
+    if not s:
+        raise Exception('Could not find header month: %s' % header)
+    current_year = date.today().year
+    day = int(s.groups()[1])
+    month = months.index(s.groups()[0]) + 1
+    year = current_year if date.today().month <= month else current_year + 1
+    return date(year, month, day)
+
+def get_header_time(header):
+    """ Returns the time corresponding to the given header. """
+    s = re.search('(\d+):(\d+)\s*(am|pm)', header, re.IGNORECASE)
+    if s:
+        hour = int(s.groups()[0])
+        minute = int(s.groups()[1])
+        if s.groups()[2].lower() == 'pm':
+            hour += 12
+        return time(hour,minute)
+    elif re.search('morning', header, re.IGNORECASE):
+        return time(10,0)
+    elif re.search('evening', header, re.IGNORECASE):
+        return time(18,30)
+    else:
+        raise Exception('Could not find header time: %s' % header)
+
+def parse_assignment(line):
+    """ Returns an assignment:assignee tuple. """
+    s = line.split(':')
+    p = [a.strip() for a in s[1].split(';')]
+    if len(p) == 1:
+        v = [a.strip() for a in p[0].split(',')]
+    else:
+        v = [[a.strip() for a in i.split(',')] for i in p]
+    return (s[0].strip(), v)
+
 def parse_schedule(lines):
     """ Parses a feed from read_feed and returns a dictionary of assignments by date. """
-    pass
+    schedule = {}
+    current_date = None
+    current_time = None
+    for line in lines:
+        if is_header(line):
+            current_date = get_header_date(line)
+            if not current_date in schedule:
+                schedule[current_date] = {}
+            current_time = get_header_time(line)
+            if not current_time in schedule[current_date]:
+                schedule[current_date][current_time] = {}
+        else:
+            n,p = parse_assignment(line)
+            schedule[current_date][current_time][n] = p
+    return schedule
+
+def flatten(soup, elements):
+    for name in elements:
+        for e in soup.findAll(name):
+            e.replaceWithChildren()
 
 def read_feed(url):
-    """ Pulls the feed down and puts it into a list of strings. """
+    """ Pulls the feed down and puts it into a sanitized list of strings. """
     feed = feedparser.parse(feed_url)
     lines = []
     for item in feed['items']:
         for contentPart in item['content']:
             soup = BeautifulSoup(contentPart['value'])
             fix_headers(soup)
-            fix_sameline(soup, ['Bass','Churchview','Sound Board'])
+            fix_sameline(soup, ['Bass','Churchview','Sound Board','Clean'])
             fix_extras(soup, ['Sound Booth'])
+            flatten(soup, ['strong','b','div'])
+            fix_orphans(soup)
             for line in get_lines(soup):
                 if len(line.strip()) > 0:
                     lines.append(line)
@@ -77,3 +148,14 @@ def read_feed(url):
 def get_schedule(url):
     """ Gets the given schedule and returns a dictionary of assignments by date. """
     return parse_schedule(read_feed(url))
+
+def dump_schedule(schedule):
+    for d in sorted(schedule.keys()):
+        print(d.isoformat())
+        for t in sorted(schedule[d].keys()):
+            print('\t' + t.isoformat())
+            for a in sorted(schedule[d][t].keys()):
+                print('\t\t%s: %s' % (a, repr(s[d][t][a])))
+
+s = get_schedule(feed_url)
+dump_schedule(s)
